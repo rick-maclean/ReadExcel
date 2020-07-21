cacb93e8e8f004620c2b0723633fc7486820dbaf
rick-maclean <rick_maclean@sil.org>
2020-07-20 22:24:05 -0500

    Start of project

---
+++ Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ReadExcel.Models;
+
+namespace ReadExcel.Controllers
+{
+    public class UserController : Controller
+    {
+        // Default GET Method
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new List<UserModel>());
+        }
+    }
+}
